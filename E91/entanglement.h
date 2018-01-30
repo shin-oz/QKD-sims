0bc21de8ee00c0166b6e418ab68436c5a386690d
shin_oz <purishin1215@gmail.com>
2018-01-30 11:48:24 +0900

    .gitignoreの作成とentanglementの修正

--- E91/entanglement.h
+++ E91/entanglement.h
@@ -1,13 +1,21 @@
 #ifndef _ENTANGLEMENT_H_
 #define _ENTANGLEMENT_H_
 
-void entanglement(int flag){
+#include <math.h>
+
+double entanglement(int flag){
+// void entanglement(int flag){
   // 2量子エンタングルメントを作るための4*4行列
   printf("here is operator to make two qubit entanglement\n");
-  int operator[4][4] = {{1,0,0,1},{0,1,1,0},{1,0,0,-1},{0,1,-1,0}};
+  double operator[4][4] = {
+    {sqrt( 2 )*1,sqrt( 2 )*0,sqrt( 2 )*0,sqrt( 2 )*1},
+    {sqrt( 2 )*0,sqrt( 2 )*1,sqrt( 2 )*1,sqrt( 2 )*0},
+    {sqrt( 2 )*1,sqrt( 2 )*0,sqrt( 2 )*0,sqrt( 2 )*-1},
+    {sqrt( 2 )*0,sqrt( 2 )*1,sqrt( 2 )*-1,sqrt( 2 )*0}
+  };
   for(int b = 0; b < 4; b++){
     for(int a = 0; a < 4; a++){
-      printf("%d",operator[a][b]);
+      printf("%f",operator[a][b]);
       if(a==3){
         printf("\n");
       }else{
@@ -18,7 +26,7 @@ void entanglement(int flag){
 
   // 2量子エンタングルメントとなる4*1行列
   printf("here is qubits to make two qubit entanglement\n");
-  int qubit[4][1];
+  double qubit[4][1];
   if(flag==0){
     // |Phi^+>
     for(int i = 0; i<4; i++){
@@ -76,22 +84,22 @@ void entanglement(int flag){
     // qubit = {0,0,0,1};
   }
   for(int i=0;i<4;i++){
-    printf("%d",qubit[i][0]);
+    printf("%f",qubit[i][0]);
     printf("\n");
   }
 
   // 2量子エンタングルメントの結果
   printf("resulut of making engtanglement|Phi^+>is below.\n");
-  int entanglement[4][1] = {0,0,0,0};
+  double entanglement[4][1] = {0,0,0,0};
   for(int a=0; a<4; a++){
-    int test = 0;
+    double test = 0;
     for(int x=0; x<4;x++){
       test += operator[x][a]*qubit[x][0];
     }
     entanglement[a][0] = test;
   }
   for(int a=0; a<4; a++){
-    printf("%d",entanglement[a][0]);
+    printf("%f",entanglement[a][0]);
     if(a==3){
       printf("\n");
     }else{
@@ -99,5 +107,11 @@ void entanglement(int flag){
     }
   }
 
+  double probability = 1/pow(sqrt(2),2.0)*100;
+  printf("%fの確率で|01> or -|10>\n",probability);
+  // int probability = 1/pow(sqrt(3),2.0)*100;
+  // printf("%dの確率で\n",probability);
+
+return entanglement[1][0];
 }
 #endif
--- E91/main.c
+++ E91/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <math.h>
+
 #include "quantum.h"
 #include "observer.h"
 #include "physical_layer.h"
@@ -8,5 +10,31 @@ int main (int argc, char *args[]) {
   printf("Hello, this is E91sim made by chanou\n");
   // observe();
   // physical();
-  entanglement(3);
+  double X = entanglement(3);
+
+  // for(int a=0; a<4; a++){
+    printf("%f\n",X);
+    // if(a==3){
+      // printf("\n");
+    // }else{
+      // printf(",");
+    // }
+  // }
+
+  // double a = pow( 4.0, 2.0 );
+  double b = sqrt( 2 );
+  double a = pow( b, 2.0 );
+  // printf( "a=%f b=%f\n", b, a );
+
+  
+  // int a[10];
+  // for(int i = 0; i<10; i++){
+  //   a[i] = i;
+  // }
+  // ran(a,10,2);
+  // for(int i = 0; i<10; i++){
+  //   printf("%d\n",a[i]);
+  // }
+
+
 }
